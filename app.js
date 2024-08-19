70130d577a6cab839391e09c2f600ee89e2cdeeb
MASHRAFI47 <mbngms@gmail.com>
2024-08-19 11:44:12 +0600

    single product view added

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iduz7rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,10 +37,10 @@ async function run() {
             const search = req.query.search;
 
             let query = {
-                productName: {$regex: search, $options: 'i'}
+                productName: { $regex: search, $options: 'i' }
             };
-            if(brand) {
-                query = {...query, brand: brand};
+            if (brand) {
+                query = { ...query, brand: brand };
             }
 
             const options = {}
@@ -54,13 +54,20 @@ async function run() {
             const search = req.query.search;
 
             let query = {
-                productName: {$regex: search, $options: 'i'}
+                productName: { $regex: search, $options: 'i' }
             };
-            if(brand) {
-                query = {...query, brand: brand}
+            if (brand) {
+                query = { ...query, brand: brand }
             }
             const count = await productsCollection.countDocuments(query);
             res.send({ count })
+        })
+
+        app.get("/product-details/:id", async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await productsCollection.findOne(query);
+            res.send(result)
         })
 
         // Send a ping to confirm a successful connection
